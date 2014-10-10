f7c49dcffcb05e3255d0efc0828980b0ff349eaf
Tony Christie <tony.christie@gmail.com>
2014-10-10 20:44:35 +0100

    Initial Commit

---
+++ 99RedBalloons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  99RedBalloons
+//
+//  Created by Tony Christie on 10/10/2014.
+//  Copyright (c) 2014 Spire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
